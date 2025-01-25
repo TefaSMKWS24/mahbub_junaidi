f8c3017d056eab4d110f1f3cedb54de8a13c16ca
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-01-25 12:03:44 -0800

    new file:   app/Http/Controllers/BarangController.php 	new file:   app/Http/Controllers/PelangganController.php 	new file:   app/Http/Controllers/SupplierController.php 	new file:   app/Http/Controllers/TransaksiController.php 	new file:   app/Http/Controllers/kasirController.php 	new file:   app/Http/Controllers/kategoriController.php 	new file:   resources/views/create.blade.php 	new file:   resources/views/edit.blade.php 	new file:   resources/views/index.blade.php

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use illuminate\support\facades\Redirect;
+use illuminate\support\facades\Validator;
+
+class BaragController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('barang.index',);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('barang.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kode_barang' => 'required',
+            'nama_barang' => 'required',
+            'harga' => 'required',
+            'stok' => 'required',
+            'kode_kategori' => 'required',
+        ]);
+
+        $data = [
+                'kode_barang' => $request->kode_barang,
+                'nama_barang' => $request->nama_barang,
+                'harga' => $request->harga,
+                'stok' => $request->stok,
+                'kode_kategori' =>$request->kode_kategori,
+        ];
+
+        DB::table('barang')->insert($data);
+        return redirect()->view('barang.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $barang =DB::table('barang')->where('kode_barang', $id)->first();
+        return view('barang.edit', compact('barang'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, String $id)
+    {
+        $request->validate([
+            'kode_barang' => 'required',
+            'nama_barang' => 'required',
+            'harga' => 'required',
+            'stok' => 'required',
+            'kode_kategori' => 'required',
+        ]);
+
+        $data = [
+                'kode_barang' => $request->kode_barang,
+                'nama_barang' => $request->nama_barang,
+                'harga' => $request->harga,
+                'stok' => $request->stok,
+                'kode_kategori' =>$request->kode_kategori,
+        ];
+
+        DB::table('barang')->where('kode_barang', $id)->update($data);
+        return redirect()->view('barang.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        DB::table('barang')->where('kode_barang', $id)->delete($data);
+        return redirect()->view('barang.index');
+    }
+}
