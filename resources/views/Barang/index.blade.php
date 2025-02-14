ead2f736f2dddd52d868bf1f60b44cf925231548
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-14 08:12:06 -0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   database/seeders/DatabaseSeeder.php
    	modified:   database/seeders/KategoriSeeder.php
    	modified:   database/seeders/PelangganSeeder.php
    	modified:   database/seeders/SupplierSeeder.php
    	modified:   resources/views/Barang/index.blade.php#	new file:
        resources/views/layout/base

--- resources/views/Barang/index.blade.php
+++ resources/views/Barang/index.blade.php
@@ -1,39 +1,53 @@
-@extends('layouts.layout')
+extends('layouts.layout')
 
-('header')
+@section('header')
 
-
-
-
-
-
+<div class="app-content -header">
+    <! - -begin::Controller- ->
+    <div class="app-controller-fluid">
+        <!--begin::Row-->
+        <div class="row">
+            <div clas="col-5m-6"><h3 class="mb-0">Barang</h3></div>
+            <div class="col-5m-6">
+                <ol class="breadcrumb float-5m-end">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Barang</li>
+                </ol>
+            </div>
+        </div>
+        <!--end::Row-->
+    </div>
+    <!-- - -end::Controller- ->
+</div>
+@endsection
 @section('content')
 <table>
     <thead>
         <tr>
             <th>No</th>
-            <th>kode</th>
-            <th>Nama</th>
+            <th>kode Barang</th>
+            <th>nama Barang</th>
             <th>kategori</th>
             <th>stok</th>
             <th>harga</th>
-            <th>aksi</th>
+            <th>Aksi</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($barang as $item)
-            <tr>
-                <td>{{ $item->id }}</td>
-                <td>{{ $item->kode_kasir }}</td>
-                <td>{{ $item->nama_barang }}</td>
-                <td>{{ $item->kategori->nama_kategori }}</td>
-                <td>{{ $item->stok}}</td>
-                <td>{{ $item->harga}}</td>
-                <td>
-                    <a href="/kasir/{{ $item->id }}/edit">Edit</a>
-                    <a href="/kasir/{{ $item->id }}/delete">Delete</a>
-                </td>
-            </tr>
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $item->kode_barang }}</td>
+            <td>{{ $item->nama_barang }}</td>
+            <td>{{ $item->kategori->nama_kategori }}</td>
+            <td>{{ $item->stok }}</td>
+            <td>{{ $item->harga }}</td>
+            <td>
+                <a href="/barang/edit/{{ $item->kode_barang }}/edit" class="btn btn-primary">Edit</a>
+                <a href="/barang/delete/{{ $item->kode_barang }}" class="btn btn-danger">hapus</a>
+            </td>
+        </tr>
         @endforeach
-    </tbody>
-    @endsection
+        </tbody>
+</table>
+@endsection
